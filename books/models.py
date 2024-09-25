cfb366d7fbc13452f305e48ba17c68c77e0799f7
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-25 15:42:40 +0300

    Create the book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from enum import Enum
 
-# Create your models here.
+
+class Cover(Enum):
+    HARD = "HARD"
+    SOFT = "SOFT"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=[(tag.value, tag.name) for tag in Cover])
+    inventory = models.PositiveIntegerField()
+    daily_fee = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"

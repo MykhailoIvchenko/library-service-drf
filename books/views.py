d08ce67908e629f8a625bb51b8cd301bfb67ec36
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-25 16:31:32 +0300

    Fix the book view

--- books/views.py
+++ books/views.py
@@ -1,14 +1,9 @@
-from rest_framework import mixins
-from rest_framework.viewsets import GenericViewSet
+from rest_framework import viewsets
 
 from books.models import Book
 from books.serializers import BookSerializer
 
 
-class BookViewSet(
-    mixins.CreateModelMixin,
-    mixins.ListModelMixin,
-    GenericViewSet,
-):
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer

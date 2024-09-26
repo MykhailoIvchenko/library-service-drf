7894b79c63af0658bdfd81de2febd76520f508ad
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-26 10:39:07 +0300

    Create the borrowing view

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+from borrowings.models import Borrowing
+from borrowings.serializers import BorrowingSerializer, BorrowingListSerializer, BorrowingDetailsSerializer
+
+
+class BorrowingViewSet(
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Borrowing.objects.select_related("books")
+    serializer_class = BorrowingSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return BorrowingListSerializer
+
+        if self.action == "retrieve":
+            return BorrowingDetailsSerializer
+
+        return BorrowingSerializer

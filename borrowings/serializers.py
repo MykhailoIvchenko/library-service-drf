d054a0cb3109961797759a9722fb3683f9128d39
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-26 10:12:12 +0300

    Create the serilizer for the borrowings list displaying

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -13,3 +13,12 @@ class BorrowingSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "actual_return_date",
         )
+
+
+class BorrowingListSerializer(BorrowingSerializer):
+    book = serializers.SlugRelatedField(read_only=True, slug_field="title")
+
+    class Meta:
+        model = Borrowing
+        fields = ("id", "book", "borrow_date", "expected_return_date", "actual_return_date")
+

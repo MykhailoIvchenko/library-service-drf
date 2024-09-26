93fe2996cfa1b8f14366f2aa79f47dcf76a0b1c1
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-26 11:29:21 +0300

    Implement the serializer for the borrowing creation

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
@@ -30,3 +31,21 @@ class BorrowingDetailsSerializer(BorrowingSerializer):
     class Meta:
         model = Borrowing
         fields = ("id", "book", "borrow_date", "expected_return_date", "actual_return_date")
+
+
+class CreateBorrowingSerializer(BorrowingSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ("book", "expected_return_date")
+
+    def validate_book(self, value):
+        if value.inventory <= 0:
+            raise ValidationError("This book is not available for borrowing.")
+        return value
+
+    def create(self, validated_data):
+        borrowing = Borrowing.objects.create(**validated_data)
+        book = validated_data['book']
+        book.inventory -= 1
+        book.save()
+        return borrowing

9d9d238045d1500a689a5a0dcd8ad1c4e2d5010c
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-26 09:52:34 +0300

    Create the borrowing model

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
-# Create your models here.
+from books.models import Book
+
+
+class Borrowing(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField()
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField()
+
+    def clean(self):
+        super().clean()
+        if self.expected_return_date < self.borrow_date:
+            raise ValidationError("The expected return date cannot be before the borrow date")
+
+        if self.actual_return_date < self.borrow_date:
+            raise ValidationError("The actual return date cannot be before the borrow date")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
